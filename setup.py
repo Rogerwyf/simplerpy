54635085832e868ab8a329c570393181ac3811d2
mhsieh808 <38347509+mhsieh808@users.noreply.github.com>
2022-02-26 17:05:38 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simplerpy",
+    version="1.0.0",
+    author=["Roger Wang", "Michelle Hsieh", "Regina-Mae Dominguez"],
+    author_email=["rogerwyf@uw.edu", "mh808@uw.edu", "rmvd@uw.edu"],
+    description="Instance in an Instant",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Rogerwyf/simplerpy",
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
