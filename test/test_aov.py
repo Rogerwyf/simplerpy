c1f6fefd4b9cc66166741fd786c7393e08ad446c
mhsieh808 <38347509+mhsieh808@users.noreply.github.com>
2022-03-07 13:14:53 -0800

    Update test_aov.py
    added test_r_obj and test_summary

--- test/test_aov.py
+++ test/test_aov.py
@@ -25,16 +25,21 @@ R = ro.r
 class AOVTestCase(unittest.TestCase):
 
   def setUp(self):
-        d1 = pd.DataFrame({'x1': [1, 2, 3, 4, 5],
-                                 'x2': [1, 1, 1, 1, 1],
-                                 'x3': [1, 0, 0, 1, 1],
-                                 'x4': [1, 2, 3, 4, 5],
-                                 'y': [2, 1, 3, 5, 4]})
+#         d1 = pd.DataFrame({'x1': [1, 2, 3, 4, 5],
+#                                  'x2': [1, 1, 1, 1, 1],
+#                                  'x3': [1, 0, 0, 1, 1],
+#                                  'x4': [1, 2, 3, 4, 5],
+#                                  'y': [2, 1, 3, 5, 4]})
 
-        self.M1R = R.aov(Formula('y~x1+x2+x3+x4'), d1)
-        self.M1P = AOV()
-        self.M1P.fit('y~x1+x2+x3+x4', d1)
-  
+#         self.M1R = R.aov(Formula('y~x1+x2+x3+x4'), d1)
+#         self.M1P = AOV()
+#         self.M1P.fit('y~x1+x2+x3+x4', d1)
+    df = pd.read_csv('Sales_Sample.csv')
+    self.M1R = R.aov(\
+                     Formula("LAST_SALE_PRICE ~ BEDS + SQFT + LOT_SIZE + BATHS"), df)
+    self.M1P = AOV()
+    self.M1P.fit('LAST_SALE_PRICE ~ BEDS + SQFT + LOT_SIZE + BATHS', df)
+
   def test_df(self):
     r = list((R.summary(self.M1R)[0])['Df'])
     r = r[:-1]
@@ -64,6 +69,21 @@ class AOVTestCase(unittest.TestCase):
     p = self.M1P.residual_se()
     self.assertEqual(r, p)
 
-if __name__ == "__main__":
-    suite = unittest.TestLoader().loadTestsFromTestCase(AOVTestCase)
-    _ = unittest.TextTestRunner().run(suite)
+ def test_r_obj(self):
+    r = self.M1R
+    p = self.M1P.r_model_obj()
+    self.assertEqual(type(r), type(p))
+
+  def test_summary(self):
+    r = self.M1R
+    p = self.M1P.summary()
+    rawOut=str(r)
+    cleanOut=""
+    textOfInterest=False
+    for line in rawOut.splitlines():
+      if line.startswith("Terms"):
+        cleanOut+=line+ "\n"
+        textOfInterest=True
+      elif textOfInterest:
+        cleanOut+=line +"\n"
+    self.assertEqual(cleanOut, p)
