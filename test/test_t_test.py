d3921dd8cfc114682509dc83fdb6a109caedf6bd
Regina Dominguez <rmvdominguez15@gmail.com>
2022-03-06 18:18:53 -0800

    test unitttest

--- test/test_t_test.py
+++ test/test_t_test.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import numpy as np
 
 from simplerpy.t_test import tTest
-from rpy2.robjects.package import importr
+from rpy2.robjects.packages import importr
 base = importr('base')
 stats = importr('stats')
 
@@ -10,7 +11,7 @@ import unittest
 class tTestCase(unittest.TestCase):
     def setUp(self):
         dataset1 = pd.DataFrame({
-            'x': [3,2,5,0,9,10]
+            'x': [3,2,5,0,9,10],
             'y': [12,3,6,10,2,4]
         })
 
@@ -38,11 +39,11 @@ class tTestCase(unittest.TestCase):
         # numpy
         self.M2R_two_np = stats.t_test(dataA, dataB)
         self.M2P_two_np = tTest()
-        self.M2P_two.fit(dataA, dataB)
+        self.M2P_two_np.fit(dataA, dataB)
 
     def test_t_with_pd_one(self):
-        R_testval = self.M1R.rx2('statistic')[0]
-        P_testval = self.M1P.tvalue()
+        R_testval = self.M1R_one.rx2('statistic')[0]
+        P_testval = self.M1P_one.tvalue()
         self.assertEqual(R_testval, P_testval)
 
     def test_t_with_np_one(self):
@@ -51,51 +52,51 @@ class tTestCase(unittest.TestCase):
         self.assertEqual(R_testval, P_testval)
 
     def test_t_with_pd_two(self):
-        R_testval = self.M2R_two.rx2('statistic')[0]
-        P_testval = self.M2P_two.tvalue()
-        self.assertEqual(R_testval, P_testval)
+        R_testval_two = self.M2R_two.rx2('statistic')[0]
+        P_testval_two = self.M2P_two.tvalue()
+        self.assertEqual(R_testval_two, P_testval_two)
 
     def test_t_with_np_two(self):
-        R_testval = self.M2R_two_np.rx2('statistic')[0]
-        P_testval = self.M2P_two_np.tvalue()
-        self.assertEqual(R_testval, P_testval)
+        R_testval_np = self.M2R_two_np.rx2('statistic')[0]
+        P_testval_np = self.M2P_two_np.tvalue()
+        self.assertEqual(R_testval_np, P_testval_np)
 
     def test_t_pvalue(self):
-        R_testval = self.M2R_two.rx2('p.value')[0]
-        P_testval = self.M2P_two.pvalue()
-        self.assertEqual(R_testval, P_testval)
+        R_pval = self.M2R_two.rx2('p.value')[0]
+        P_pval = self.M2P_two.pvalue()
+        self.assertEqual(R_pval, P_pval)
 
     def test_t_df(self):
-        R_testval = self.M2R_two.rx2('parameter')[0]
-        P_testval = self.M2P_two.df()
-        self.assertEqual(R_testval, P_testval)
+        R_df = self.M2R_two.rx2('parameter')[0]
+        P_df = self.M2P_two.df()
+        self.assertEqual(R_df, P_df)
 
     def test_t_ci(self):
-        R_testval = self.M2R_two.rx2('conf.int')[0]
-        P_testval = self.M2P_two.ci()
-        self.assertEqual(R_testval, P_testval)
+        R_ci = self.M2R_two.rx2('conf.int')[:2]
+        P_ci = self.M2P_two.ci()
+        self.assertEqual(R_ci.any(), P_ci.any())
 
     def test_t_estimate(self):
-        R_testval = self.M2R_two.rx2('estimate')[0]
-        P_testval = self.M2P_two.estimate()
-        self.assertEqual(R_testval, P_testval)
+        R_est = self.M2R_two.rx2('estimate')
+        P_est = self.M2P_two.estimate()
+        self.assertEqual(R_est.any(), P_est.any())
 
     def test_t_stderror(self):
-        R_testval = self.M2R_two.rx2('stderr')[0]
-        P_testval = self.M2P_two.stderror()
-        self.assertEqual(R_testval, P_testval)
+        R_stder = self.M2R_two.rx2('stderr')[0]
+        P_stder = self.M2P_two.stderror()
+        self.assertEqual(R_stder, P_stder)
 
 
     def test_t_alternative(self):
-        R_testval = self.M2R_two.rx2('alternative')[0]
-        P_testval = self.M2P_two.alternative()
-        self.assertEqual(R_testval, P_testval)
+        R_alt = self.M2R_two.rx2('alternative')[0]
+        P_alt = self.M2P_two.alternative()
+        self.assertEqual(R_alt, P_alt)
 
     def test_t_method(self):
-        R_testval = self.M2R_two.rx2('method')[0]
-        P_testval = self.M2P_two.method()
-        self.assertEqual(R_testval, P_testval)
+        R_method = self.M2R_two.rx2('method')[0]
+        P_method = self.M2P_two.method()
+        self.assertEqual(R_method, P_method)
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     suite = unittest.TestLoader().loadTestsFromTestCase(tTestCase)
     _ = unittest.TextTestRunner().run(suite)
