dcc419674f698cc2d6b3c57ad6a35de4d19f8d68
mhsieh808 <38347509+mhsieh808@users.noreply.github.com>
2022-03-06 17:24:05 -0800

    Update aov.py
    wrote class for aov()

--- simplerpy/aov.py
+++ simplerpy/aov.py
@@ -24,7 +24,6 @@ from rpy2.robjects import pandas2ri
 pandas2ri.activate()
 R=ro.r
 
-#class WIP
 class AOV:
   def __init__(self):
     """
@@ -34,46 +33,109 @@ class AOV:
     """
     self._model=None
   
-  def fit(self, formula):
+  def fit(self, f, df):
     """
     Fits the aov model with input formula using aov() in R
     through the package rpy2.
-    :param formula: formula used for aov() i.e. "y~x"
+    :param f: formula used for aov() i.e. "y~x"
+    :param df: the data frame containing variables of interest
     :return: None, assign the R model object to self._model
     """
-
+    self._model=R.aov(Formula(f), df)
   
+  def r_model_obj(self):
+    """
+    Returns the fitted R model object.
+
+    :return: a R model object
+    """
+    return self._model
+
+  def df(self):
+    """
+    Returns the degrees of freedom on the feature variables of the fitted model,
+    retrieved from the R model object.
+
+    :return: list of integers
+    """
+    sumOut = R.summary(self._model)[0]
+    degFree = list(sumOut['Df'])
+    return degFree[:-1]
+
+  def df_residual(self):
+    """
+    Returns the degrees of freedom on residuals of the fitted model, retrieved
+    from the R model object.
+
+    :return: an integer
+    """
+    df = self._model.rx2('df.residual')[0]
+    return df
+
+  def sum_of_squares(self):
+    """
+
+    Returns the sum of squares based on the fitted model, retrieved from the R 
+    model object.
+
+    :return: a list of float numbers
+    """
+    sumOut = R.summary(self._model)[0]
+    SSR = list(sumOut['Sum Sq'])
+    return SSR[:-1]
+
+  def sum_of_squares_res(self):
+    """
+    Returns the sum of squares of the residuals based on the fitted model, 
+    retrieved from the R model object.
+
+    :return: a float number
+
+    """
+    SSRes = self._model.rx2('residuals')
+    SSRes = sum(list(map(lambda x:pow(x,2), SSRes)))
+    return SSRes
 
 
-#finished part
-def simpAOV(text, df):
-  """
-  Arguments: text, df
-    text: the formula you want to pass into aov
-    df: the data frame 
-  """
-  model=R.aov(Formula(text), df)
-  vars=list(pd.DataFrame(model.rx2('model')).columns) #extracting headers
-  sumOut=R.summary(model)[0]
-  SSR=list(sumOut['Sum Sq'])
-  output="Terms"+"\n" + "              " + "\t"
-  maxLength=len("{:e}".format(SSR[0]))+1  
-  for i in range(1, len(vars)):
-    x=vars[i]
-    output+= f"{x:>{maxLength}}"
-  output+=f"{'Residuals':>{maxLength}}"+"\n" +"Sum of Squares"+"\t"
-  SSResid=(float(SSR[len(SSR)-1]))
-  for i in SSR: #extracting SSR values
-    x=SSR.pop(0)
-    xText="{:e}".format(x)
-    SSR.append(xText)
-    output+=f"{xText:>{maxLength}}"
-  output+="\n"+"Deg. of Freedom"+"\t"
-  degFree=list(sumOut['Df'])
-  for i in range(len(degFree)):
-    output+=f"{str(int(degFree[i])):>{maxLength}}"
-  RSE=math.sqrt(SSResid/degFree[len(degFree)-1]) 
-  #calculating residual standard error
-  output+= "\n"+"\n"+"Residual standard error: "+str(round(RSE, 1)) +"\n"+\
-  "Estimated effects may be unbalanced"
-  return output
+
+
+  def residual_se(self):
+    """
+    Returns the residual standard errors from the fitted model, retrieved from
+    the R model object
+
+    :return: a float number
+    """
+    RSE = math.sqrt(AOV.sum_of_squares_res(self)/AOV.df_residual(self)) 
+    return RSE
+
+
+
+  def summary(self):
+    """
+    Prints a cleaned summary output for the fitted aov model like in R.
+
+    :return: None
+    """
+    output = "Terms"+"\n" + "              " + "\t"
+    maxLength = len("{:e}".format(AOV.sum_of_squares(self)[0]))+1
+    model = AOV.r_model_obj(self)
+    headers = list(model.rx2('model').columns)
+    for i in range(1, len(headers)):
+      output+=f"{headers[i]:>{maxLength}}"
+    output+=f"{'Residuals':>{maxLength}}" + "\n" +"Sum of Squares" +"\t"
+    SSR = AOV.sum_of_squares(self)
+    for i in range(len(SSR)):
+      output+=f"{'{:e}'.format(SSR[i]):>{maxLength}}"
+    print(AOV.sum_of_squares_res(self))
+    output+=f"{'{:e}'.format(AOV.sum_of_squares_res(self)):>{maxLength}}"+"\n" \
+    +"Deg. of Freedom"+"\t"
+    degFree = AOV.df(self)
+    degRes = AOV.df_residual(self)
+    for i in range(len(degFree)):
+      output+= f"{str(int(degFree[i])):>{maxLength}}"
+    output+=f"{str(AOV.df_residual(self)):>{maxLength}}" + "\n" + "\n" + \
+    "Residual standard error: "
+    RSE = AOV.residual_se(self)
+    output+=str(round(RSE, 1)) + "\n" + "Estimated effects may be unbalanced"
+    print(output)
